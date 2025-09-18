66dbc0ceee03995528ef90b6ad0f59a2a5a3607c
Abu Saleh Md Siam <abusalehmdsiam@gmail.com>
2025-09-19 04:18:31 +0600

    api fix

--- benzine_prokashon_client/src/Hooks/useAxiosSecure.jsx
+++ benzine_prokashon_client/src/Hooks/useAxiosSecure.jsx
@@ -3,8 +3,8 @@ import { useNavigate } from "react-router-dom";
 import useAuth from "./useAuth";
 
 const axiosSecure = axios.create({
-  // baseURL: "https://benzine-prokashon-g41m.onrender.com/",
-  baseURL: "http://localhost:5000/",
+  baseURL: "https://benzine-prokashon-g41m.onrender.com/",
+  // baseURL: "http://localhost:5000/",
 });
 
 // Flag to prevent adding interceptors multiple times
